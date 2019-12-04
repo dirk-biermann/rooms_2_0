52bd0eaddc4e652bf8f650a69dae30856a7f5069
Biermann, Dirk <biermannd@mediamarktsaturn.com>
2019-12-04 13:38:26 +0100

    add sound - reorganize

--- js/basic.js
+++ js/basic.js
@@ -11,3 +11,29 @@ function changeCSSColor(id) {
     }
 }
 
+function changeBtnSelection(id){
+    let control_btn = document.getElementById("control-btn");
+    let btn_list = control_btn.getElementsByClassName("btn");
+
+    [...btn_list].forEach( (btn, ind) => {
+            if( btn.id.startsWith("chg-color") ) {
+                btn.classList.toggle("selected", ind === id+1 );
+            }
+        });
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
+}
+
